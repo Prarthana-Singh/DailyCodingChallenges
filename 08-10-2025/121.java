191f7a5fee9cc018e91f710b3294441118322288
Prarthana Singh <143778905+Prarthana-Singh@users.noreply.github.com>
2025-10-08 00:02:02 +0530

    Create 121.java

---
+++ 08-10-2025/121.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int max = 0;
+        int min = prices[0];
+
+        for(int i = 0; i < prices.length; i++){
+
+            if(min > prices[i]) min = prices[i];
+
+            if(prices[i] > min){
+                max = Math.max(max, prices[i] - min);
+            }
+        }
+        return max;
+    }
+}
