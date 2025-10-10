09dd07820dfd1c4f3dfc95097a1554bf4d6aa096
Prarthana Singh <143778905+Prarthana-Singh@users.noreply.github.com>
2025-10-10 22:22:33 +0530

    Create Remove Outermost Parentheses.java

---
+++ 10-10-2025/Remove Outermost Parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder result = new StringBuilder();
+        int count = 0;
+        for(char ch: s.toCharArray()){
+
+            if(ch == '('){
+                if(count > 0 ) result.append('(');
+                count++;
+            }
+            if(ch == ')'){
+                count--;
+                if(count > 0 ) result.append(')');
+            }
+        }
+        return result.toString();
+    }
+}
